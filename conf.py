9bd2f60b7623679ad38fb9f027d2b7b9e9e5ff7a
Serik <aybatov.serik@gmail.com>
2022-03-10 21:39:18 +0300

    Add some points

--- conf.py
+++ conf.py
@@ -1,4 +1,7 @@
 
 
 
-interestedIds = ["002209","002580","002535","002281"]
+closestIds = ["002209","002580","002535","002281"]
+additionalIds = ["002274","002272","002485","002769"]
+
+interestedIds = closestIds + additionalIds
